94ddb23ce4f0a0398077228aeb312579cea0ce76
kogcyc <kogcyc@gmail.com>
2024-09-27 08:05:39 -0700

    Update kassette.js

--- kassette.js
+++ kassette.js
@@ -1,9 +1,6 @@
 // kassette.js
-const kassette = [
+export const kassette = [
     { path: 'one/1.md', className: 'one' },
     { path: 'two/2.md', className: 'two' },
     { path: 'three/3.md', className: 'three' }
 ];
-
-// Export the kassette array so it can be imported elsewhere
-export { kassette };
