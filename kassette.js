1e59926865f1684199dbc30962ddd9e32144185d
kogcyc <kogcyc@gmail.com>
2024-09-27 15:36:17 -0700

    Update kassette.js

--- kassette.js
+++ kassette.js
@@ -1,6 +1,6 @@
 // kassette.js
 export const kassette = [
-    { path: 'one/1.md', classNames: 'three zoom' },
+    { path: 'one/1.md', classNames: 'zoom' },
     { path: 'two/2.md', classNames: '' },
     { path: 'three/3.md', classNames: '' }
 ];
