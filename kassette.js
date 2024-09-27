8178c1e6ae2c344f5539bb54eb81fd385d86ddee
kogcyc <kogcyc@gmail.com>
2024-09-27 07:52:41 -0700

    Create kassette.js

---
+++ kassette.js
@@ -0,0 +1,9 @@
+// kassette.js
+const kassette = [
+    { path: 'one/1.md', className: 'one' },
+    { path: 'two/2.md', className: 'two' },
+    { path: 'three/3.md', className: 'three' }
+];
+
+// Export the kassette array so it can be imported elsewhere
+export { kassette };
