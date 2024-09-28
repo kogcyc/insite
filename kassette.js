f84777f8b87a30e669c046ac4afbfea4ae0e881d
kogcyc <kogcyc@gmail.com>
2024-09-28 15:44:00 -0700

    Update kassette.js

--- kassette.js
+++ kassette.js
@@ -1,5 +1,7 @@
 // kassette.js
-export const kassette = [
+// export const 
+    
+module.exports kassette = [
     { path: 'one/1.md', classNames: 'zoom' },
     { path: 'two/2.md', classNames: '' },
     { path: 'three/3.md', classNames: '' }
